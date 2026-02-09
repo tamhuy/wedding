b78232ad199871203b9fe432d874f0ca3c0fec72
QT <quangtamh95@gmail.com>
2026-02-09 22:32:15 +0100

    responsive navbar

--- src/sections/Program.tsx
+++ src/sections/Program.tsx
@@ -3,13 +3,13 @@ import { useTranslation } from "react-i18next";
 export default function Program() {
     const { t } = useTranslation();
     return (
-        <>
+        <section id="program">
          <h1>{t("program.title")}</h1>
          <p>{t("program.ceremony")}</p>
          <p>{t("program.cocktailHour")}</p>
          <p>{t("program.dinner")}</p>
          <p>{t("program.party")}</p>
          <p>{t("program.farewell")}</p>
-        </>
+        </section>
     );
 }
