b1200172b64857461898a69ad47c4aac545d06c2
QT <quangtamh95@gmail.com>
2026-02-14 14:05:14 +0100

    RSVP styling

--- src/sections/RSVP.tsx
+++ src/sections/RSVP.tsx
@@ -1,18 +1,19 @@
 import { Button } from "@mui/material";
 import { useTranslation } from "react-i18next";
+import LaunchIcon from '@mui/icons-material/Launch';
 
 export default function RSVP() {
     const { t } = useTranslation();
     return (
-        <>
-         <h1>{t("rsvp.title")}</h1>
-         <p>{t("rsvp.message")}</p>
-         <p>{t("rsvp.appreciation")}</p>
-         <p>{t("rsvp.deadline")}</p>
-         <Button variant="contained" color="primary">
+        <div>
+         <h2>{t("rsvp.title")}</h2>
+         <p className="my-4">{t("rsvp.message")}</p>
+         <p className="my-4">{t("rsvp.appreciation")}</p>
+         <p className="my-4">{t("rsvp.deadline")}</p>
+         <Button className="" variant="contained" target="_blank" color="primary" href="https://forms.google.com" endIcon={<LaunchIcon />}>
             {t("rsvp.buttonLabel")}
          </Button>
-         <p>{t("rsvp.contact")}</p>
-        </>
+         <p className="my-4">{t("rsvp.contact")}</p>
+        </div>
     );
 }
