1722327900c2289d9d5fd99a54d391bdc638af64
QT <quangtamh95@gmail.com>
2026-02-15 00:14:56 +0100

    Button styling

--- src/sections/RSVP.tsx
+++ src/sections/RSVP.tsx
@@ -9,7 +9,7 @@ export default function RSVP() {
          <h2 className="text-white">{t("rsvp.title")}</h2>
          <p className="text-white my-4">{t("rsvp.message")}</p>
          <p className="text-white my-4">{t("rsvp.deadline")}</p>
-         <Button className="!bg-secondary/70 hover:!bg-secondary/50 !text-black " size="large" variant="contained" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSf9ZIxOMC361IIPEtcWIlTC6dDxh72qSUlAZBGSZRDAp2gSKw/viewform?usp=dialog" endIcon={<LaunchIcon />}>
+         <Button className="!bg-primary hover:!bg-secondary/50 !text-white " size="large" variant="contained" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSf9ZIxOMC361IIPEtcWIlTC6dDxh72qSUlAZBGSZRDAp2gSKw/viewform?usp=dialog" endIcon={<LaunchIcon />}>
             {t("rsvp.buttonLabel")}
          </Button>
          <p className="text-white my-4">{t("rsvp.appreciation")}</p>
