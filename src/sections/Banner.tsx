059161f3d4fe695f4c4db040a932dc78c46e2583
QT <quangtamh95@gmail.com>
2026-02-14 23:10:39 +0100

    mobile styling

--- src/sections/Banner.tsx
+++ src/sections/Banner.tsx
@@ -6,16 +6,15 @@ export function Banner() {
 
   return (
     <div
-      className="banner h-screen flex flex-col items-center text-black p-4 text-center -mt-14 sm:-mt-16"
+      className="banner h-screen flex flex-col items-center text-black pt-20 md:pt-[23vh] p-4 text-center -mt-14 sm:-mt-16"
       style={{
-        paddingTop: '23vh',
         backgroundImage: `url(${banner})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}
     >
-      <h1 className="tracking-wider m-8 text-shadow-lg/75 text-white text-shadow-black whitespace-pre-line md:whitespace-normal">{t('banner.title')}</h1>
-      <h1 className="text-5xl tracking-wider m-4 text-shadow-lg/75 text-white text-shadow-black">{t('banner.date')}</h1>
+      <h1 className="tracking-wider m-2 leading-20 md:leading-none md:m-8 text-shadow-lg/75 text-white text-shadow-black whitespace-pre-line md:whitespace-normal">{t('banner.title')}</h1>
+      <h1 className="text-3xl md:text-5xl tracking-wider m-4 text-shadow-lg/75 text-white text-shadow-black">{t('banner.date')}</h1>
     </div>
   );
 }
