4f747e9121cf8fad0977a8ed82d8c57c41f603da
QT <quangtamh95@gmail.com>
2026-02-14 21:25:47 +0100

    Norsk v2

--- src/sections/FAQ.tsx
+++ src/sections/FAQ.tsx
@@ -29,7 +29,12 @@ export default function FAQ() {
                     <FaqSubSection title={t("practicalInfo.dressCode")} text={t("practicalInfo.dressCodeSub")} />
                     <FaqSubSection title={t("practicalInfo.childFree")} text={t("practicalInfo.childFreeSub")} />
                     <FaqSubSection title={t("practicalInfo.accommodation")} text={t("practicalInfo.accommodationSub")} />
-                    <FaqSubSection title={t("practicalInfo.transportation")} text={t("practicalInfo.transportationSub")} />
+                    <div className="my-6">
+                        <div>
+                            <h3>{t("practicalInfo.transportation")}</h3>
+                            <p className="whitespace-pre-line">{t("practicalInfo.transportationSub")} <a className="underline" href="ruter.no" >Ruter.no</a>{t("practicalInfo.transportationSub2")}</p>
+                        </div>
+                    </div>
                     <FaqSubSection title={t("practicalInfo.weather")} text={t("practicalInfo.weatherSub")} />
                     <FaqSubSection title={t("practicalInfo.wishingWell")} text={t("practicalInfo.wishingWellSub")} />
                     <FaqSubSection title={t("practicalInfo.contribution")} text={t("practicalInfo.contributionSub")} />
