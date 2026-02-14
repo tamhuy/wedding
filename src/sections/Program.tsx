6701dd706fb2188370cdccc3d44191602c2317ce
QT <quangtamh95@gmail.com>
2026-02-14 23:03:26 +0100

    SVG icons

--- src/sections/Program.tsx
+++ src/sections/Program.tsx
@@ -1,20 +1,36 @@
 import { useTranslation } from "react-i18next";
-import DoorSlidingOutlinedIcon from '@mui/icons-material/DoorSlidingOutlined';
-import type { SvgIconComponent } from '@mui/icons-material';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import LocalBarOutlinedIcon from '@mui/icons-material/LocalBarOutlined';
-import RestaurantOutlinedIcon from '@mui/icons-material/RestaurantOutlined';
-import CakeOutlinedIcon from '@mui/icons-material/CakeOutlined';
-import CelebrationOutlinedIcon from '@mui/icons-material/CelebrationOutlined';
-import SoupKitchenOutlinedIcon from '@mui/icons-material/SoupKitchenOutlined';
-import WavingHandOutlinedIcon from '@mui/icons-material/WavingHandOutlined';
+import Rings from "../assets/icons/rings.svg?react";
+import Dinner from "../assets/icons/dinner.svg?react";
+import Cake from "../assets/icons/cake.svg?react";
+import Dance from "../assets/icons/dance.svg?react";
+import Welcome from "../assets/icons/welcome.svg?react";
+import Cocktail from "../assets/icons/cocktail.svg?react";
+import Snack from "../assets/icons/snack.svg?react";
+import Bye from "../assets/icons/bye.svg?react";
 
+
+import SvgIcon, { type SvgIconProps } from '@mui/material/SvgIcon';
+import type { ComponentType } from "react";
 
 type ProgramSubSectionProps = {
     title: string,
     text: string,
-    Icon: SvgIconComponent,
+    Icon: React.ComponentType<SvgIconProps>,
 }
+
+ function createSvgIcon(SvgComponent: ComponentType<React.SVGProps<SVGSVGElement>>) {
+  return function Icon(props: SvgIconProps) {
+    return <SvgIcon component={SvgComponent} inheritViewBox {...props} />;
+  };
+}
+const RingsIcon = createSvgIcon(Rings);
+const DinnerIcon = createSvgIcon(Dinner);
+const CakeIcon = createSvgIcon(Cake);
+const DanceIcon = createSvgIcon(Dance);
+const WelcomeIcon = createSvgIcon(Welcome);
+const CocktailIcon = createSvgIcon(Cocktail);
+const SnackIcon = createSvgIcon(Snack);
+const ByeIcon = createSvgIcon(Bye);
 
 function ProgramSubSection({title, text, Icon}: ProgramSubSectionProps) {
    return (
@@ -33,14 +49,14 @@ export default function Program() {
     return (
         <section id="program">
          <h2>{t("program.title")}</h2>
-         <ProgramSubSection Icon={DoorSlidingOutlinedIcon} title={t("program.doorsOpen")} text={t("program.doorsOpenSub")} />
-         <ProgramSubSection Icon={FavoriteBorderOutlinedIcon} title={t("program.ceremony")} text={t("program.ceremonySub")} />
-         <ProgramSubSection Icon={LocalBarOutlinedIcon} title={t("program.cocktailHour")} text={t("program.cocktailHourSub")} />
-         <ProgramSubSection Icon={RestaurantOutlinedIcon} title={t("program.dinner")} text={t("program.dinnerSub")} />
-         <ProgramSubSection Icon={CakeOutlinedIcon} title={t("program.cakeCutting")} text={t("program.cakeCuttingSub")} />
-         <ProgramSubSection Icon={CelebrationOutlinedIcon} title={t("program.party")} text={t("program.partySub")} />
-         <ProgramSubSection Icon={SoupKitchenOutlinedIcon} title={t("program.snacks")} text={t("program.snacksSub")} />
-         <ProgramSubSection Icon={WavingHandOutlinedIcon} title={t("program.farewell")} text={t("program.farewellSub")} />
+         <ProgramSubSection Icon={WelcomeIcon} title={t("program.doorsOpen")} text={t("program.doorsOpenSub")} />
+         <ProgramSubSection Icon={RingsIcon} title={t("program.ceremony")} text={t("program.ceremonySub")} />
+         <ProgramSubSection Icon={CocktailIcon} title={t("program.cocktailHour")} text={t("program.cocktailHourSub")} />
+         <ProgramSubSection Icon={DinnerIcon} title={t("program.dinner")} text={t("program.dinnerSub")} />
+         <ProgramSubSection Icon={CakeIcon} title={t("program.cakeCutting")} text={t("program.cakeCuttingSub")} />
+         <ProgramSubSection Icon={DanceIcon} title={t("program.party")} text={t("program.partySub")} />
+         <ProgramSubSection Icon={SnackIcon} title={t("program.snacks")} text={t("program.snacksSub")} />
+         <ProgramSubSection Icon={ByeIcon} title={t("program.farewell")} text={t("program.farewellSub")} />
         </section>
     );
 }
